bc1925eb69818c2e9e5d6c268e0034eb77155476
Alyssa Drobatz <drobatzalyssa@gmail.com>
2018-03-20 16:35:13 -0500

    initial state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+//CATEGORY PAGE - NO DETAILS
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      products: [{
+        id: '',
+        size: '',
+        designer: '',
+        price: 0,
+        name: ''
+      }]
+      }
 
+  }
   componentDidMount(){
     fetch('https://api-dev.luxurygaragesale.com/v1/products/')
     .then(response =>{
       return response.json()
     })
     .then(products =>{
-      console.log(products)
+      products.hits.map(product =>{
+        this.setState({
+          products: [...this.state.products, {
+            id: product._id,
+            size: product._source.brandSize,
+            designer: product._source.designer,
+            price: product._source.price,
+            name: product._source.name,
+          }]
+        })
+      })
     })
       .catch(err =>{
         console.error(err)
