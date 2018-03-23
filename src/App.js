f06f1134142e3ec9ad5450e4f13c4d68e578ad94
Alyssa Drobatz <drobatzalyssa@gmail.com>
2018-03-23 14:35:43 -0500

    clean up

--- src/App.js
+++ src/App.js
@@ -5,53 +5,53 @@ import {Link} from 'react-router-dom';
 
 //CATEGORY PAGE - NO DETAILS
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      products: [],
-      offset: 0,
-      limit: 12,
-      firstPage: false
-    }
-    this.back = this.back.bind(this)
-    this.next = this.next.bind(this)
-    this.fetchProducts = this.fetchProducts.bind(this)
-  }
-  componentDidMount(){
-    this.fetchProducts()
-  }
-//is only taking one - figure out how to take 12 but not prev 12
+      constructor(){
+        super();
+        this.state = {
+          products: [],
+          offset: 0,
+          limit: 24,
+          firstPage: false
+        }
+        this.back = this.back.bind(this)
+        this.next = this.next.bind(this)
+        this.fetchProducts = this.fetchProducts.bind(this)
+      }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.offset !== this.state.offset){
-      this.fetchProducts()
-  }
-}
+      componentDidMount(){
+        this.fetchProducts()
+      }
 
-  fetchProducts(){
-  fetch(`https://api-dev.luxurygaragesale.com/v1/products/?offset=${this.state.offset}&limit=${this.state.limit}`)
-    .then(response =>{
-      return response.json()
-    })
-    .then(products =>{
-      var productArr = []
-      products.hits.map(product =>{
-        productArr.push({
-            id: product._id,
-            size: product._source.brandSize,
-            designer: product._source.designer,
-            price: product._source.price,
-            name: product._source.name,
-            sku: product._source.sku
+      componentDidUpdate(prevProps, prevState) {
+        if (prevState.offset !== this.state.offset){
+          this.fetchProducts()
+          window.scrollTo(0, 0)
+        }
+      }
+
+      fetchProducts(){
+        fetch(`https://api-dev.luxurygaragesale.com/v1/products/?offset=${this.state.offset}&limit=${this.state.limit}`)
+          .then(response =>{
+            return response.json()
           })
-        return this.setState({products: productArr})
-      })
-    })
-      .catch(err =>{
-        console.error(err)
-      })
-
-  }
+          .then(products =>{
+            var productArr = []
+            products.hits.map(product =>{
+              productArr.push({
+                  id: product._id,
+                  size: product._source.brandSize,
+                  designer: product._source.designer,
+                  price: product._source.price,
+                  name: product._source.name,
+                  sku: product._source.sku
+                })
+              return this.setState({products: productArr})
+            })
+          })
+          .catch(err =>{
+            console.error(err)
+          })
+      }
 
       back(){
         var decrementedOffset = this.state.offset - this.state.limit
@@ -59,26 +59,14 @@ class App extends Component {
       }
 
       next(){
-        // var nextPage = Number(this.props.match.params.productPage) + 1
-        // this.props.history.push(`/${nextPage}`)
         var incrementedOffset = this.state.offset + this.state.limit
         this.setState({offset: incrementedOffset})
       }
 
       render() {
         console.log('render', this.state)
-        var {limit} = this.state
         var totalProducts = this.state.products
-        // var currentPage = this.props.match.params.productPage
-        var pageNumbers = []
-        var pageCounter = 0
 
-        for (var i = 0; i < totalProducts.length; i++){
-          if (i % limit === 0){
-            pageCounter++
-            pageNumbers.push(pageCounter)
-          }
-        }
         var lastPage = this.state.offset === 0
         return (
                 <div>
