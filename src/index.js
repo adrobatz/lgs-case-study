76d96c0865e1cef83e8bcb4e7320e97f33ed2edb
Alyssa Drobatz <drobatzalyssa@gmail.com>
2018-03-23 10:40:23 -0500

    edit read me

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route, Router, BrowserRouter} from 'react-router-dom'
+import {Route, Redirect, BrowserRouter, Switch} from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
     <BrowserRouter>
+    <Switch>
       <Route path='/:productPage' component={App} />
+      <Route exact path='/' render={()=>
+        <Redirect to='/1'/>
+      } />
+      </Switch>
     </BrowserRouter>
     ),
     document.getElementById('root'));
